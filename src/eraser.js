c09f7000602e9d3f55fd1d65f1a333b022c28912
Iliyan Peychev <iliyan.peychev@gmail.com>
2016-12-27 20:41:43 +0100

    Make eraser more precice
    - in case of a single point, make two lines
    - performance improvements

--- src/eraser.js
+++ src/eraser.js
@@ -22,7 +22,7 @@ class Eraser {
     this._detachListeners();
   }
 
-  draw(event) {
+  erase(event) {
     if (!this._isErasing) {
       return;
     }
@@ -58,23 +58,29 @@ class Eraser {
   start(event) {
     this._isErasing = true;
 
-    this._lastPoint = Utils.getPointFromEvent(event, this._canvasElement);
+    let curPoint = Utils.getPointFromEvent(event, this._canvasElement);
+
+    let matchingShapes = this._getMatchingShapes(curPoint);
+
+    if (matchingShapes.length) {
+      this._config.callback(matchingShapes);
+    }
   }
 
   _attachListeners() {
-    this._startListener = this.start.bind(this);
     this._cancelListener = this.cancel.bind(this);
-    this._drawListener = this.draw.bind(this);
+    this._eraseListener = this.erase.bind(this);
     this._finishListener = this.finish.bind(this);
+    this._startListener = this.start.bind(this);
 
     if (Utils.isTouchDevice()) {
       this._canvasElement.addEventListener('touchcancel', this._cancelListener, {passive: true});
       this._canvasElement.addEventListener('touchend', this._finishListener, {passive: true});
-      this._canvasElement.addEventListener('touchmove', this._drawListener);
+      this._canvasElement.addEventListener('touchmove', this._eraseListener);
       this._canvasElement.addEventListener('touchstart', this._startListener, {passive: true});
     } else {
       this._canvasElement.addEventListener('mousedown', this._startListener);
-      this._canvasElement.addEventListener('mousemove', this._drawListener);
+      this._canvasElement.addEventListener('mousemove', this._eraseListener);
       this._canvasElement.addEventListener('mouseup', this._finishListener);
     }
   }
@@ -82,35 +88,38 @@ class Eraser {
   _checkShapeMatching(curPoint, shape) {
     let points = shape.points;
 
-    for (let i = 0, j = 1; i < points.length; i++, j++) {
-      let point1 = points[i];
-      let point2 = points[j];
+    let p1 = [curPoint[0] - 10, curPoint[1]];
+    let p2 = [curPoint[0] + 10, curPoint[1]];
+    let p3 = [curPoint[0], curPoint[1] - 10];
+    let p4 = [curPoint[0], curPoint[1] + 10];
+    let point1;
+    let point2;
 
-      if (!point2) {
-        point2 = [point1[0] + 10, point1[1] + 10];
+    let i = 0, j = 1;
+
+    do {
+      if (points.length === 1) {
+        point1 = [points[0][0] - 5, points[0][1] - 5];
+        point2 = [points[0][0] + 5, points[0][1] + 5];
+      } else {
+        point1 = points[i];
+        point2 = points[j];
       }
 
-      let p1 = [], p2 = [], p3 = [], p4 = [];
+      let intersection = this._isIntersecting(p1, p2, point1, point2);
 
-      p1[0] = [curPoint[0] - 10];
-      p1[1] = [curPoint[1]];
+      if (!intersection) {
+        intersection = this._isIntersecting(p3, p4, point1, point2);
+      }
 
-      p2[0] = [curPoint[0] + 10];
-      p2[1] = [curPoint[1]];
-
-      p3[0] = curPoint[0];
-      p3[1] = curPoint[1] - 10;
-
-      p4[0] = curPoint[0];
-      p4[1] = curPoint[1] + 10;
-
-      let option1 = this._isIntersecting(p1, p2, point1, point2);
-      let option2 = this._isIntersecting(p3, p4, point1, point2);
-
-      if (option1 || option2) {
+      if (intersection) {
         return true;
       }
-    }
+
+      i++, j++;
+    } while (i < points.length - 1);
+
+    return false;
   }
 
   _counterClockwise(p1, p2, p3) {
@@ -119,11 +128,11 @@ class Eraser {
 
   _detachListeners() {
     this._canvasElement.removeEventListener('mousedown', this._startListener);
-    this._canvasElement.removeEventListener('mousemove', this._drawListener);
+    this._canvasElement.removeEventListener('mousemove', this._eraseListener);
     this._canvasElement.removeEventListener('mouseup', this._finishListener);
     this._canvasElement.removeEventListener('touchcancel', this._cancelListener);
     this._canvasElement.removeEventListener('touchend', this._finishListener);
-    this._canvasElement.removeEventListener('touchmove', this._drawListener);
+    this._canvasElement.removeEventListener('touchmove', this._eraseListener);
     this._canvasElement.removeEventListener('touchstart', this._startListener);
   }
 
